7d3d7cf85e85e4bbe52bc41fe0ed2d01fac2c228
Nils Hendriks <nhendrik@student.codam.nl>
2026-08-31 17:53:38 +0200

    Character selections

---
+++ codam-game-jam-8-2026/Start/CharacterSelectButton.gd
@@ -0,0 +1,17 @@
+extends Button
+
+func _on_pressed() -> void:
+	match self.name:
+		"Cook":
+			print("Cook")
+		"Miller":
+			print("Miller")
+		"Tanner":
+			print("Tanner")
+		"Smith":
+			print("Smith")
+		"Tailor":
+			print("Tailor")
+		"Abbot":
+			print("Abbot")
+	get_tree().change_scene_to_file("res://Game/Game.tscn")
--- codam-game-jam-8-2026/Start/start_button.gd
+++ codam-game-jam-8-2026/Start/start_button.gd
@@ -3,4 +3,4 @@ extends Button
 
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://Game/Game.tscn")
+	get_tree().change_scene_to_file("res://Start/CharacterSelect.tscn")
